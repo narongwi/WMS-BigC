6d51c36dd40f6d171966c7408946027153a3fb01
deftsoft2021 <deftsoft2021@gmail.com>
2021-09-02 18:23:42 +0700

    fix distribute clear screen after put line

--- snaps.wms.pda/PDA/lib/screens/distribution/models/prepdistribute_model.dart
+++ snaps.wms.pda/PDA/lib/screens/distribution/models/prepdistribute_model.dart
@@ -12,19 +12,7 @@ class DistributeFilter {
   String tflow;
   String deviceID;
 
-  DistributeFilter(
-      {this.spcarea,
-      this.routeno,
-      this.huno,
-      this.preptype,
-      this.prepno,
-      this.prepdate,
-      this.thcode,
-      this.spcorder,
-      this.spcarticle,
-      this.dateassign,
-      this.tflow,
-      this.deviceID});
+  DistributeFilter({this.spcarea, this.routeno, this.huno, this.preptype, this.prepno, this.prepdate, this.thcode, this.spcorder, this.spcarticle, this.dateassign, this.tflow, this.deviceID});
 
   DistributeFilter.fromJson(Map<String, dynamic> json) {
     spcarea = json['spcarea'];
@@ -32,13 +20,11 @@ class DistributeFilter {
     huno = json['huno'];
     preptype = json['preptype'];
     prepno = json['prepno'];
-    prepdate =
-        json['prepdate'] == null ? null : DateTime.parse(json["prepdate"]);
+    prepdate = json['prepdate'] == null ? null : DateTime.parse(json["prepdate"]);
     thcode = json['thcode'];
     spcorder = json['spcorder'];
     spcarticle = json['spcarticle'];
-    dateassign =
-        json['dateassign'] == null ? null : DateTime.parse(json["dateassign"]);
+    dateassign = json['dateassign'] == null ? null : DateTime.parse(json["dateassign"]);
     tflow = json['tflow'];
     deviceID = json['deviceID'];
   }
@@ -50,13 +36,11 @@ class DistributeFilter {
     data['huno'] = this.huno;
     data['preptype'] = this.preptype;
     data['prepno'] = this.prepno;
-    data['prepdate'] =
-        this.prepdate != null ? prepdate.toIso8601String() : prepdate;
+    data['prepdate'] = this.prepdate != null ? prepdate.toIso8601String() : prepdate;
     data['thcode'] = this.thcode;
     data['spcorder'] = this.spcorder;
     data['spcarticle'] = this.spcarticle;
-    data['dateassign'] =
-        this.dateassign != null ? prepdate.toIso8601String() : prepdate;
+    data['dateassign'] = this.dateassign != null ? prepdate.toIso8601String() : prepdate;
     data['tflow'] = this.tflow;
     data['deviceID'] = this.deviceID;
     return data;
@@ -85,27 +69,7 @@ class Distribution {
   String preptypename;
   String przone;
 
-  Distribution(
-      {this.orgcode,
-      this.site,
-      this.depot,
-      this.spcarea,
-      this.routeno,
-      this.huno,
-      this.preptype,
-      this.prepno,
-      this.prepdate,
-      this.priority,
-      this.thcode,
-      this.spcorder,
-      this.spcarticle,
-      this.tflow,
-      this.capacity,
-      this.thname,
-      this.picker,
-      this.preppct,
-      this.preptypename,
-      this.przone});
+  Distribution({this.orgcode, this.site, this.depot, this.spcarea, this.routeno, this.huno, this.preptype, this.prepno, this.prepdate, this.priority, this.thcode, this.spcorder, this.spcarticle, this.tflow, this.capacity, this.thname, this.picker, this.preppct, this.preptypename, this.przone});
 
   Distribution.fromJson(Map<String, dynamic> json) {
     orgcode = json['orgcode'];
@@ -116,8 +80,7 @@ class Distribution {
     huno = json['huno'];
     preptype = json['preptype'];
     prepno = json['prepno'];
-    prepdate =
-        json['prepdate'] == null ? null : DateTime.parse(json["prepdate"]);
+    prepdate = json['prepdate'] == null ? null : DateTime.parse(json["prepdate"]);
     priority = json['priority'];
     thcode = json['thcode'];
     spcorder = json['spcorder'];
@@ -141,8 +104,7 @@ class Distribution {
     data['huno'] = this.huno;
     data['preptype'] = this.preptype;
     data['prepno'] = this.prepno;
-    data['prepdate'] =
-        this.prepdate != null ? prepdate.toIso8601String() : prepdate;
+    data['prepdate'] = this.prepdate != null ? prepdate.toIso8601String() : prepdate;
     data['priority'] = this.priority;
     data['thcode'] = this.thcode;
     data['spcorder'] = this.spcorder;
@@ -241,17 +203,13 @@ class DistrbInfo {
       this.przone});
 
   DistrbInfo.fromJson(Map<String, dynamic> json) {
-    dateassign =
-        json['dateassign'] == null ? null : DateTime.parse(json["dateassign"]);
-    datestart =
-        json['datestart'] == null ? null : DateTime.parse(json["datestart"]);
+    dateassign = json['dateassign'] == null ? null : DateTime.parse(json["dateassign"]);
+    datestart = json['datestart'] == null ? null : DateTime.parse(json["datestart"]);
     dateend = json['dateend'] == null ? null : DateTime.parse(json["dateend"]);
     deviceID = json['deviceID'];
-    datecreate =
-        json['datecreate'] == null ? null : DateTime.parse(json["datecreate"]);
+    datecreate = json['datecreate'] == null ? null : DateTime.parse(json["datecreate"]);
     accncreate = json['accncreate'];
-    datemodify =
-        json['datemodify'] == null ? null : DateTime.parse(json["datemodify"]);
+    datemodify = json['datemodify'] == null ? null : DateTime.parse(json["datemodify"]);
     accnmodify = json['accnmodify'];
     procmodify = json['procmodify'];
     if (json['lines'] != null) {
@@ -277,8 +235,7 @@ class DistrbInfo {
     huno = json['huno'];
     preptype = json['preptype'];
     prepno = json['prepno'];
-    prepdate =
-        json['prepdate'] == null ? null : DateTime.parse(json["prepdate"]);
+    prepdate = json['prepdate'] == null ? null : DateTime.parse(json["prepdate"]);
     priority = json['priority'];
     thcode = json['thcode'];
     spcorder = json['spcorder'];
@@ -294,18 +251,13 @@ class DistrbInfo {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['dateassign'] =
-        this.dateassign != null ? dateassign.toIso8601String() : dateassign;
-    data['datestart'] =
-        this.datestart != null ? datestart.toIso8601String() : datestart;
-    data['dateend'] =
-        this.dateend != null ? dateend.toIso8601String() : dateend;
+    data['dateassign'] = this.dateassign != null ? dateassign.toIso8601String() : dateassign;
+    data['datestart'] = this.datestart != null ? datestart.toIso8601String() : datestart;
+    data['dateend'] = this.dateend != null ? dateend.toIso8601String() : dateend;
     data['deviceID'] = this.deviceID;
-    data['datecreate'] =
-        this.datecreate != null ? datecreate.toIso8601String() : datecreate;
+    data['datecreate'] = this.datecreate != null ? datecreate.toIso8601String() : datecreate;
     data['accncreate'] = this.accncreate;
-    data['datemodify'] =
-        this.datemodify != null ? datemodify.toIso8601String() : datemodify;
+    data['datemodify'] = this.datemodify != null ? datemodify.toIso8601String() : datemodify;
     data['accnmodify'] = this.accnmodify;
     data['procmodify'] = this.procmodify;
     if (this.lines != null) {
@@ -328,8 +280,7 @@ class DistrbInfo {
     data['huno'] = this.huno;
     data['preptype'] = this.preptype;
     data['prepno'] = this.prepno;
-    data['prepdate'] =
-        this.prepdate != null ? prepdate.toIso8601String() : prepdate;
+    data['prepdate'] = this.prepdate != null ? prepdate.toIso8601String() : prepdate;
     data['priority'] = this.priority;
     data['thcode'] = this.thcode;
     data['spcorder'] = this.spcorder;
@@ -480,8 +431,7 @@ class DistrbLine {
     picker = json['picker'];
     datepick = json['datepick'];
     devicecode = json['devicecode'];
-    datecreate =
-        json['datecreate'] == null ? null : DateTime.parse(json["datecreate"]);
+    datecreate = json['datecreate'] == null ? null : DateTime.parse(json["datecreate"]);
     accncreate = json['accncreate'];
     accnmodify = json['accnmodify'];
     procmodify = json['procmodify'];
@@ -501,8 +451,7 @@ class DistrbLine {
     qtypuorder = json['qtypuorder'];
     qtypuops = json['qtypuops'];
     tflow = json['tflow'];
-    datemodify =
-        json['datemodify'] == null ? null : DateTime.parse(json["datemodify"]);
+    datemodify = json['datemodify'] == null ? null : DateTime.parse(json["datemodify"]);
     unitprep = json['unitprep'];
     unitname = json['unitname'];
     qtyskuops = json['qtyskuops'];
@@ -532,25 +481,20 @@ class DistrbLine {
     data['qtyvolumeops'] = this.qtyvolumeops;
     data['batchno'] = this.batchno;
     data['lotno'] = this.lotno;
-    data['datemfg'] =
-        this.datemfg != null ? datemfg.toIso8601String() : datemfg;
-    data['dateexp'] =
-        this.dateexp != null ? dateexp.toIso8601String() : dateexp;
+    data['datemfg'] = this.datemfg != null ? datemfg.toIso8601String() : datemfg;
+    data['dateexp'] = this.dateexp != null ? dateexp.toIso8601String() : dateexp;
     data['serialno'] = this.serialno;
     data['picker'] = this.picker;
-    data['datepick'] =
-        this.datepick != null ? datepick.toIso8601String() : datepick;
+    data['datepick'] = this.datepick != null ? datepick.toIso8601String() : datepick;
     data['devicecode'] = this.devicecode;
-    data['datecreate'] =
-        this.datecreate != null ? datecreate.toIso8601String() : datecreate;
+    data['datecreate'] = this.datecreate != null ? datecreate.toIso8601String() : datecreate;
     data['accncreate'] = this.accncreate;
     data['accnmodify'] = this.accnmodify;
     data['procmodify'] = this.procmodify;
     data['rtoskuofpu'] = this.rtoskuofpu;
     data['thcode'] = this.thcode;
     data['taskno'] = this.taskno;
-    data['daterec'] =
-        this.daterec != null ? daterec.toIso8601String() : daterec;
+    data['daterec'] = this.daterec != null ? daterec.toIso8601String() : daterec;
     data['inagrn'] = this.inagrn;
     data['ingrno'] = this.ingrno;
     data['orgcode'] = this.orgcode;
@@ -563,8 +507,7 @@ class DistrbLine {
     data['qtypuorder'] = this.qtypuorder;
     data['qtypuops'] = this.qtypuops;
     data['tflow'] = this.tflow;
-    data['datemodify'] =
-        this.datemodify != null ? datemodify.toIso8601String() : datemodify;
+    data['datemodify'] = this.datemodify != null ? datemodify.toIso8601String() : datemodify;
     data['unitprep'] = this.unitprep;
     data['unitname'] = this.unitname;
     data['qtyskuops'] = this.qtyskuops;
