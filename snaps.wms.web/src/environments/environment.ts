c0bcb2fa5cbb1209fd3aa22f812800c3911de8b5
deftsoft2021 <deftsoft2021@gmail.com>
2021-09-16 18:39:25 +0700

    fix print hu empty  type

--- snaps.wms.web/src/environments/environment.ts
+++ snaps.wms.web/src/environments/environment.ts
@@ -1,4 +1,3 @@
-
 export const environment = {
   production: false,
   urlapiAuth: 'http://desktop-2dakhvc:5001',
@@ -6,21 +5,18 @@ export const environment = {
   // urlapiAdmn: 'http://localhost:63738',
   urlapiInbnd: 'http://desktop-2dakhvc:4320',
   urlapiTask: 'http://desktop-2dakhvc:4420',
-  urlapiInvt: 'http://localhost:61645',
-  urlapiOubnd: 'http://desktop-2dakhvc:4620',
+  urlapiInvt: 'http://desktop-2dakhvc:4520',
+  urlapiOubnd: 'http://localhost:23120',
   urlapiReport: 'http://desktop-2dakhvc:4720',
   urlapiDocument: 'http://desktop-2dakhvc:4820',
-  urlapiExssource: 'http://desktop-2dakhvc:4920'
-    // urlapiAuth: 'http://bgcdwmsasn-ap01:5001',
-    // urlapiAdmn: 'http://bgcdwmsasn-ap01:4220',
-    // urlapiInbnd: 'http://localhost:38547',
-    // urlapiTask: 'http://bgcdwmsasn-ap01:4420',
-    // urlapiInvt: 'http://bgcdwmsasn-ap01:4520',
-    // urlapiOubnd: 'http://localhost:23120',
-    // urlapiReport: 'http://bgcdwmsasn-ap01:4720',
-    // urlapiDocument: 'http://bgcdwmsasn-ap01:4820',
-    // urlapiExssource: 'http://bgcdwmsasn-ap01:4920'
+  urlapiExssource: 'http://desktop-2dakhvc:4920',
+  // urlapiAuth: 'http://bgcdwmsasn-ap01:5001',
+  // urlapiAdmn: 'http://bgcdwmsasn-ap01:4220',
+  // urlapiInbnd: 'http://localhost:38547',
+  // urlapiTask: 'http://bgcdwmsasn-ap01:4420',
+  // urlapiInvt: 'http://bgcdwmsasn-ap01:4520',
+  // urlapiOubnd: 'http://localhost:23120',
+  // urlapiReport: 'http://bgcdwmsasn-ap01:4720',
+  // urlapiDocument: 'http://bgcdwmsasn-ap01:4820',
+  // urlapiExssource: 'http://bgcdwmsasn-ap01:4920'
 };
-
-
-
